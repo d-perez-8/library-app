81428b5de429e8b1b90f2894fc949432d6f55f33
Danny Perez <xdanny28x@gmail.com>
2023-04-07 18:10:03 -0700

    google auth having problems

--- config/passport.js
+++ config/passport.js
@@ -2,7 +2,9 @@ const passport = require('passport');
 const refresh = require('passport-oauth2-refresh');
 const axios = require('axios');
 const { Strategy: LocalStrategy } = require('passport-local');
-const { OAuth2Strategy: GoogleStrategy } = require('passport-google-oauth');
+// const { OAuth2Strategy: GoogleStrategy } = require('passport-google-oauth');
+// const { OAuthStrategy } = require('passport-oauth');
+// const { OAuth2Strategy } = require('passport-oauth');
 const _ = require('lodash');
 const moment = require('moment');
 const mongoose = require("mongoose");
@@ -60,72 +62,72 @@ passport.use(new LocalStrategy({ usernameField: 'email' }, (email, password, don
 /**
  * Sign in with Google.
  */
-const googleStrategyConfig = new GoogleStrategy({
-  clientID: process.env.GOOGLE_ID,
-  clientSecret: process.env.GOOGLE_SECRET,
-  callbackURL: '/auth/google/callback',
-  passReqToCallback: true
-}, (req, accessToken, refreshToken, params, profile, done) => {
-  if (req.user) {
-    User.findOne({ google: profile.id }, (err, existingUser) => {
-      if (err) { return done(err); }
-      if (existingUser && (existingUser.id !== req.user.id)) {
-        req.flash('errors', { msg: 'There is already a Google account that belongs to you. Sign in with that account or delete it, then link it with your current account.' });
-        done(err);
-      } else {
-        User.findById(req.user.id, (err, user) => {
-          if (err) { return done(err); }
-          user.google = profile.id;
-          user.tokens.push({
-            kind: 'google',
-            accessToken,
-            accessTokenExpires: moment().add(params.expires_in, 'seconds').format(),
-            refreshToken,
-          });
-          user.profile.name = user.profile.name || profile.displayName;
-          user.profile.gender = user.profile.gender || profile._json.gender;
-          user.profile.picture = user.profile.picture || profile._json.picture;
-          user.save((err) => {
-            req.flash('info', { msg: 'Google account has been linked.' });
-            done(err, user);
-          });
-        });
-      }
-    });
-  } else {
-    User.findOne({ google: profile.id }, (err, existingUser) => {
-      if (err) { return done(err); }
-      if (existingUser) {
-        return done(null, existingUser);
-      }
-      User.findOne({ email: profile.emails[0].value }, (err, existingEmailUser) => {
-        if (err) { return done(err); }
-        if (existingEmailUser) {
-          req.flash('errors', { msg: 'There is already an account using this email address. Sign in to that account and link it with Google manually from Account Settings.' });
-          done(err);
-        } else {
-          const user = new User();
-          user.email = profile.emails[0].value;
-          user.google = profile.id;
-          user.tokens.push({
-            kind: 'google',
-            accessToken,
-            accessTokenExpires: moment().add(params.expires_in, 'seconds').format(),
-            refreshToken,
-          });
-          user.profile.name = profile.displayName;
-          user.profile.gender = profile._json.gender;
-          user.profile.picture = profile._json.picture;
-          user.save((err) => {
-            done(err, user);
-          });
-        }
-      });
-    });
-  }
-});
-passport.use('google', googleStrategyConfig);
-refresh.use('google', googleStrategyConfig);
+// const googleStrategyConfig = new GoogleStrategy({
+//   clientID: process.env.GOOGLE_ID,
+//   clientSecret: process.env.GOOGLE_SECRET,
+//   callbackURL: '/auth/google/callback',
+//   passReqToCallback: true
+// }, (req, accessToken, refreshToken, params, profile, done) => {
+//   if (req.user) {
+//     User.findOne({ google: profile.id }, (err, existingUser) => {
+//       if (err) { return done(err); }
+//       if (existingUser && (existingUser.id !== req.user.id)) {
+//         req.flash('errors', { msg: 'There is already a Google account that belongs to you. Sign in with that account or delete it, then link it with your current account.' });
+//         done(err);
+//       } else {
+//         User.findById(req.user.id, (err, user) => {
+//           if (err) { return done(err); }
+//           user.google = profile.id;
+//           user.tokens.push({
+//             kind: 'google',
+//             accessToken,
+//             accessTokenExpires: moment().add(params.expires_in, 'seconds').format(),
+//             refreshToken,
+//           });
+//           user.profile.name = user.profile.name || profile.displayName;
+//           user.profile.gender = user.profile.gender || profile._json.gender;
+//           user.profile.picture = user.profile.picture || profile._json.picture;
+//           user.save((err) => {
+//             req.flash('info', { msg: 'Google account has been linked.' });
+//             done(err, user);
+//           });
+//         });
+//       }
+//     });
+//   } else {
+//     User.findOne({ google: profile.id }, (err, existingUser) => {
+//       if (err) { return done(err); }
+//       if (existingUser) {
+//         return done(null, existingUser);
+//       }
+//       User.findOne({ email: profile.emails[0].value }, (err, existingEmailUser) => {
+//         if (err) { return done(err); }
+//         if (existingEmailUser) {
+//           req.flash('errors', { msg: 'There is already an account using this email address. Sign in to that account and link it with Google manually from Account Settings.' });
+//           done(err);
+//         } else {
+//           const user = new User();
+//           user.email = profile.emails[0].value;
+//           user.google = profile.id;
+//           user.tokens.push({
+//             kind: 'google',
+//             accessToken,
+//             accessTokenExpires: moment().add(params.expires_in, 'seconds').format(),
+//             refreshToken,
+//           });
+//           user.profile.name = profile.displayName;
+//           user.profile.gender = profile._json.gender;
+//           user.profile.picture = profile._json.picture;
+//           user.save((err) => {
+//             done(err, user);
+//           });
+//         }
+//       });
+//     });
+//   }
+// });
+// passport.use('google', googleStrategyConfig);
+// refresh.use('google', googleStrategyConfig);
 
 /**
  * Login Required middleware.
