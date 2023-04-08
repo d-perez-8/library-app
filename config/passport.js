e73cc68460bd80b251ab2028c18bd92184745970
Danny Perez <xdanny28x@gmail.com>
2023-04-07 17:50:32 -0700

    removed OAuth2 strats

--- config/passport.js
+++ config/passport.js
@@ -1,10 +1,8 @@
 const passport = require('passport');
 const refresh = require('passport-oauth2-refresh');
 const axios = require('axios');
-const LocalStrategy = require("passport-local").Strategy;
+const { Strategy: LocalStrategy } = require('passport-local');
 const { OAuth2Strategy: GoogleStrategy } = require('passport-google-oauth');
-const { OAuthStrategy } = require('passport-oauth');
-const { OAuth2Strategy } = require('passport-oauth');
 const _ = require('lodash');
 const moment = require('moment');
 const mongoose = require("mongoose");
@@ -19,28 +17,29 @@ passport.deserializeUser((id, done) => {
   User.findById(id, (err, user) => done(err, user));
 });
 
-passport.use(
-  new LocalStrategy({ usernameField: "email" }, (email, password, done) => {
-    User.findOne({ email: email.toLowerCase() }, (err, user) => {
+
+/**
+ * Sign in locally.
+ */
+
+passport.use(new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
+  User.findOne({ email: email.toLowerCase() }, (err, user) => {
+    if (err) { return done(err); }
+    if (!user) {
+      return done(null, false, { msg: `Email ${email} not found.` });
+    }
+    if (!user.password) {
+      return done(null, false, { msg: 'Your account was registered using a sign-in provider. To enable password login, sign in using a provider, and then set a password under your user profile.' });
+    }
+    user.comparePassword(password, (err, isMatch) => {
       if (err) { return done(err); }
-      if (!user) {
-        return done(null, false, { msg: `Email ${email} not found.` });
+      if (isMatch) {
+        return done(null, user);
       }
-      if (!user.password) {
-        return done(null, false, {
-          msg: "Your account was registered using a sign-in provider. To enable password login, sign in using a provider, and then set a password under your user profile.",
-        });
-      }
-      user.comparePassword(password, (err, isMatch) => {
-        if (err) { return done(err); }
-        if (isMatch) {
-          return done(null, user);
-        }
-        return done(null, false, { msg: "Invalid email or password." });
-      });
+      return done(null, false, { msg: 'Invalid email or password.' });
     });
-  })
-);
+  });
+}));
 
 /**
  * OAuth Strategy Overview
@@ -56,6 +55,7 @@ passport.use(
  *       - If there is, return an error message.
  *       - Else create a new account.
  */
+
 
 /**
  * Sign in with Google.
